9ed96ccd19b8bad516da6df2f83ba639321f4f82
Daniel Hatanaka <hatanakadaniel@gmail.com>
2021-08-16 12:35:25 -0300

    aula1

---
+++ src/main/java/com/dio/BankApi.java
@@ -0,0 +1,18 @@
+package com.dio;
+
+import com.dio.entity.User;
+import com.dio.service.StatementService;
+
+import java.util.UUID;
+
+public class BankApi {
+
+    public static void main(String[] args) {
+        final User userA = new User(UUID.randomUUID(), "Usuario 1");
+        final User userB = new User(UUID.randomUUID(), "Usuario 2");
+        final StatementService statementService = new StatementService(userA, userB);
+
+        statementService.findAllByUser(userA).forEach(System.out::println);
+        statementService.findAllByUser(userB).forEach(System.out::println);
+    }
+}
